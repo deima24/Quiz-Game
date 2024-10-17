f0786157667c8f6ac15c547f50d6459c74b1084e
deima24 <111892514+deima24@users.noreply.github.com>
2024-10-17 15:00:51 +0100

    have issue with the questions

---
+++ js/questions.js
@@ -0,0 +1,62 @@
+let questions = [
+
+    {
+        numb: 1,
+        question: "What is number 1?",
+        answer: "A. 1",
+        options: [
+            "A. 1",
+            "B. 2",
+            "C. 3",
+            "D. 4"
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "What is number 2?",
+        answer: "B. 2",
+        options: [
+            "A. 1",
+            "B. 2",
+            "C. 3",
+            "D. 4"
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "What is number 3?",
+        answer: "C. 3",
+        options: [
+            "A. 1",
+            "B. 2",
+            "C. 3",
+            "D. 4"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "What is numbner 4?",
+        answer: "D. 4",
+        options: [
+            "A. 1",
+            "B. 2",
+            "C. 3",
+            "D. 4"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "What is number 5?",
+        answer: "A. 1",
+        options: [
+            "A. 1",
+            "B. 2",
+            "C. 3",
+            "D. 4"
+        ]
+    }
+];
