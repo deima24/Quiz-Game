be45a455fd5b63368929b4ca62065877693a4272
deima24 <111892514+deima24@users.noreply.github.com>
2024-10-30 18:30:42 +0000

    added quiz guide

--- js/questions.js
+++ js/questions.js
@@ -2,61 +2,61 @@ let questions= [
 
     {
         numb: 1,
-        question: "What is number 1?",
-        answer: "A. 1",
+        question: "Which mammal is known for being able to glide through the air?",
+        answer: "B) Flying Squirrel",
         options: [
-            "A. 1",
-            "B. 2",
-            "C. 3",
-            "D. 4"
+            "A) Kangaroo",
+            "B) Flying Squirrel",
+            "C) Rabbit",
+            "D) Beaver"
         ]
     },
 
     {
         numb: 2,
-        question: "What is number 2?",
-        answer: "B. 2",
+        question: "What is the largest species of shark?",
+        answer: "C) Whale Shark",
         options: [
-            "A. 1",
-            "B. 2",
-            "C. 3",
-            "D. 4"
+            "A) Great White Shark",
+            "B) Hammerhead Shark",
+            "C) Whale Shark",
+            "D) Tiger Shark"
         ]
     },
 
     {
         numb: 3,
-        question: "What is number 3?",
-        answer: "C. 3",
+        question: "Which bird is famous for its bright pink feathers?",
+        answer: "B) Flamingo",
         options: [
-            "A. 1",
-            "B. 2",
-            "C. 3",
-            "D. 4"
+            "A) Peacock",
+            "B) Flamingo",
+            "C) Parrot",
+            "D) Ostrich"
         ]
     },
 
     {
         numb: 4,
-        question: "What is numbner 4?",
-        answer: "D. 4",
+        question: "What type of animal is a Komodo dragon?",
+        answer: "C) Reptile",
         options: [
-            "A. 1",
-            "B. 2",
-            "C. 3",
-            "D. 4"
+            "A) Mammal",
+            "B) Amphibian",
+            "C) Reptile",
+            "D) Fish"
         ]
     },
 
     {
         numb: 5,
-        question: "What is number 5?",
-        answer: "A. 1",
+        question: "Which animal is known for having a pouch to carry its young?",
+        answer: "C) Kangaroo",
         options: [
-            "A. 1",
-            "B. 2",
-            "C. 3",
-            "D. 4"
+            "A) Elephant",
+            "B) Penguin",
+            "C) Kangaroo",
+            "D) Panda"
         ]
     }
 ];
